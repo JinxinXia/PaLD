a2d7a136314a7bd35830aa2a4b8b32465b1af1f2
Jinxin Xia <48334389+JellyHeart@users.noreply.github.com>
2020-04-29 13:14:54 -0400

    test get contribution matrix function

--- contribution_test.m
+++ contribution_test.m
@@ -1,12 +1,12 @@
 clear;
 T = readtable('distance.csv');
 
-%distance = textscan(a, '%s %s %s %s', 'delimiter', ',', 'CollectOutput',true);
+
 cell_dist = T{:,:};
 b = cell_dist(2:end,2:end);
-%distance = cell2mat(b);
+
 
 class(b)
 size(b)
 
-C = get_contribution_matrix(b,1);
+%C = get_contribution_matrix(b,1);
