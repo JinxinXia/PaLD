fe932fe98e9665cf646e7e66256fa1158b71a08b
Jinxin Xia <48334389+JellyHeart@users.noreply.github.com>
2020-04-29 13:23:48 -0400

    use random matrix as distance matrix

--- contribution_test.m
+++ contribution_test.m
@@ -1,12 +1,30 @@
 clear;
-T = readtable('distance.csv');
+%T = readtable('distance.csv');
 
 
-cell_dist = T{:,:};
-b = cell_dist(2:end,2:end);
+%cell_dist = T{:,:};
+%b = cell_dist(2:end,2:end);
 
 
-class(b)
-size(b)
+%class(b)
+%size(b)
 
+%B = [b{:,:}];
+
+%class(B)
 %C = get_contribution_matrix(b,1);
+
+
+clear;
+rng(12);
+d = rand(1,500,500);
+
+C = get_contribution_matrix(d,1);
+
+
+
+
+
+
+
+
