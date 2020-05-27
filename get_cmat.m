d684d056971d8df0e656f31fb7d1763ea5b09366
Jinxin Xia <48334389+JellyHeart@users.noreply.github.com>
2020-05-27 13:31:18 -0400

    UPDATE

--- get_cmat.m
+++ get_cmat.m
@@ -13,33 +13,9 @@ end
 n = size(D,1);
 C = zeros(n);
 
-% sort the upper triangular of distance matrix D on each row by a ascending
-% order
-sort_D = sort(triu(D),2);
 
-sort_D_indices = zeros(n);
 % get the sorted indices matrix 
-for i = 1:n
-    for j = 1:n
-        % get the original index in distance matrix
-        index = find(D(i,:) == sort_D(i,j));
-        index_size = size(index,2);
-        
-        % check if there are same distance between pairs of points
-        if index_size == 1
-            sort_D_indices(i,j) = index;
-        else
-            sort_D_indices(i,j:j+index_size) = index;
-        end
-        
-        % skip index_size of loops
-        if index_size > 0
-            index_size = index_size - 1;
-            continue;
-        end
-        
-    end
-end
+sort_D_indices = get_sorted_indices(D);
 
 
 
@@ -51,13 +27,7 @@ end
 for x = 1:(n-1)
     for y = (x+1):n
         
-        [~,uxy_size] = find(sort_D(x,:) == D(x,y));
-        D(x,y)
-        dx = D(x,:);
-        dy = D(y,:);
-        ux = find(dx <= beta*D(x,y));  
-        uy = find(dy <= beta*D(x,y));
-        b = zeros(1,n);
+        
         b(1,ux) = 1;
         b(1,uy) = 1;
 
