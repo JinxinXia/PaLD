f310c88bf5a20d9861753d40fe1d97a8cd613c3c
Jinxin Xia <48334389+JellyHeart@users.noreply.github.com>
2020-06-23 15:12:21 -0400

    Update sort_mat_contribute.m

--- sort_mat_contribute.m
+++ sort_mat_contribute.m
@@ -43,6 +43,9 @@ for i = 1:(n-1)
         % row, uy_bound give the index where d(x,y) lives in the sorted
         % distance indices matrix
         
+        
+        % analysis the computational cost
+        
         uy_bound = find(sort_D_indices(y,:) == x);
         uy = sort_D_indices(y,1:uy_bound-1);
         
