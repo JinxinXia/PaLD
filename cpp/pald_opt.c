4cada3603adc144c9dca1334c6b176d7430052e6
Grey Ballard <ballard@wfu.edu>
2020-12-16 15:28:49 -0500

    started to edit opt code, but still much more to do

--- cpp/pald_opt.c
+++ cpp/pald_opt.c
@@ -4,25 +4,37 @@
 #include <math.h>
 #include <float.h>
 
-void optimal_matmul_block(double *C, double *D, double beta, int edge_len, const int b)
+/*
+params
+D    in  distance matrix: D(x,y) is distance between x and y (symmetric)
+beta in  conflict focus parameter: z is in focus of (x,y) if 
+         min(d(z,x),d(z,y)) <= beta * d(x,y)
+n    in  number of points
+C    out cohesion matrix: C(x,z) is z's support for x
+b    in  blocking parameter for cache efficiency
+*/
+void pald_opt(double *D, double beta, int n, double* C, const int b)
 {
-    int i, j;
-    for (i = 0; i < edge_len; i += b)
-    {
-        int Ib = min(i + b - 1, edge_len);
-        for (j = i; j < edge_len; j += b)
-        {
-            int Jb = min(j + b - 1, edge_len);
+    // pre-allocate conflict focus block
+    double *Uij = calloc(b * b, sizeof(double));
 
-            int dim_row = min(b, edge_len - i + 1);
-            int dim_col = min(b, edge_len - j + 1);
-            double *Uij = calloc(dim_col * dim_row, sizeof(double));
-            double Dij = D[Ib * edge_len + Jb];
+    // loop over blocks of points I = (i,...,i+b-1)
+    for (int i = 0; i < n; i += b)
+    {
+        int Ib = min(i + b - 1, n);
+        // loop over blocks of points J = (j,...,j+b-1)
+        for (j = i; j < n; j += b)
+        {
+            int Jb = min(j + b - 1, n);
+
+            int dim_row = min(b, n - i + 1);
+            int dim_col = min(b, n - j + 1);
+            double Dij = D[Ib * n + Jb];
             int k;
 
-            for (k = 0; k < edge_len; k++)
+            for (k = 0; k < n; k++)
             {
-                if (fmin(D[Ib * edge_len + k], D[k * edge_len + Jb]) < Dij)
+                if (fmin(D[Ib * n + k], D[k * n + Jb]) < Dij)
                 {
                     int others;
                     for (others = 0; others < dim_row * dim_col; others++)
@@ -39,7 +51,7 @@ void optimal_matmul_block(double *C, double *D, double beta, int edge_len, const
                 int k;
 
                 //FIXME
-                /*for (k = 0; i < edge_len; k++)
+                /*for (k = 0; i < n; k++)
                 {
                     C[Ib * dim_row + k] +=
                 }*/
@@ -50,16 +62,19 @@ void optimal_matmul_block(double *C, double *D, double beta, int edge_len, const
             }
         }
     }
+
+    // free up cache block
+    free(Uij);
 }
 
 int main(int argc, char **argv)
 {
 
-    int edge_len = 4;
-    double *C = calloc(edge_len * edge_len, sizeof(double));
+    int n = 4;
+    double *C = calloc(n * n, sizeof(double));
 
     double D[] = {0, 1, 2, 3, 1, 0, 4, 5, 2, 4, 0, 6, 3, 5, 6, 0};
-    orig_contribute(C, D, 1, edge_len, 3);
+    pald_opt(D, 1, n, C, 3);
 
     free(C);
 }
