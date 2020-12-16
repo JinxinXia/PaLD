7d8d7007e0c4fd5af9b04b236678cf19a1c83249
Grey Ballard <ballard@wfu.edu>
2020-12-16 14:22:09 -0500

    renaming matlab files

--- matlab/contribution_test.m
+++ matlab/contribution_test.m
@@ -14,7 +14,7 @@ D = D - diag(diag(D));
 fprintf('their method, running on dimension %d\n', n)
 
 tic
-[C1,F] = orig_contribute(D,1);
+[C1,F] = pald_orig(D,1);
 toc
 
 
@@ -26,12 +26,12 @@ toc
 % where M is the cache size
 fprintf('block method, same dimension, using block size %d\n', b1)
 tic
-[C2,U] = matmul_block(D,1,b1);
+[C2,U] = pald_block(D,1,b1);
 toc 
 fprintf('error in blocked method is %g\n', norm(C1-C2))
 
 fprintf('optimal block method, same dimension, using block size %d\n', b2)
 tic
-[C3,U1] = optimal_matmul_block(D,1,b2);
+[C3,U1] = pald_opt(D,1,b2);
 toc 
 fprintf('error in optimal blocked method is %g\n', norm(C1-C3))
