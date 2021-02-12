c001a35c5d2e90f10545ad91f9ceddd0a3d6c587
Grey Ballard <ballard@wfu.edu>
2021-02-12 21:10:12 +0000

    added parallel version of opt code and new driver to test performance of parallel kernel, updated Makefile accordingly

---
+++ cpp/PaLD_par_test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#include "kernels.h"
+#include "utils.h"
+
+int main(int argc, char **argv) {
+
+    //initializing testing environment spec
+    int n, b, t, i;
+
+    // initialize timers
+    double start, time_seq, time_par;
+    
+    if ((argc != 4) || !(n = atoi(argv[1])) || !(b = atoi(argv[2])) || !(t = atoi(argv[3])) ) {
+        fprintf(stderr, "Usage: ./name mat_dim block_size num_threads\n");
+        exit(-1);
+    }
+    unsigned int num_gen = n * n;
+
+    double *C1 = calloc(num_gen, sizeof(double));
+    double *C2 = calloc(num_gen, sizeof(double));
+    double *D = malloc(sizeof(double) * num_gen);
+    dist_mat_gen2D(D, n, 1, 10*n, 12345, '2');
+
+    //computing C with parallel algorithm
+    start = omp_get_wtime();
+    pald_opt_par(D, 1, n, C1, b, t);
+    time_par = omp_get_wtime() - start;
+
+    //computing C with sequential alg  
+    start = omp_get_wtime();
+    pald_opt(D, 1, n, C2, b);
+    time_seq = omp_get_wtime() - start;
+
+    // compute max norm error between two cohesion matrices
+    double d, maxdiff = 0.;
+    for (i = 0; i < num_gen; i++) {
+        d = fabs(C1[i]-C2[i]);
+        maxdiff = d > maxdiff ? d : maxdiff;
+    }
+    printf("Maximum difference: %1.1e \n", maxdiff);
+
+    printf("Seq time: %.3fs\nPar time: %.3fs\n",time_seq,time_par);
+    printf("Parallel speedup is %f, efficiency is %2.1f\n",time_seq/time_par,time_seq/time_par/t*100);
+
+    free(D);
+    free(C2);
+    free(C1);
+}
--- cpp/kernels.h
+++ cpp/kernels.h
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <omp.h>
 
 // pald algorithms
 void pald_orig(double *D, double beta, int n, double *C);
 void pald_opt(double *D, double beta, int n, double *C, const int b);
+void pald_opt_par(double *D, double beta, int n, double *C, const int b, int num_threads);
 
 #endif //PALD_KERNELS_H
