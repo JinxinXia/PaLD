b473a2801dfedb1871b96660857bba655819b126
JinxinXia <xiaj19@wfu.edu>
2021-04-19 14:51:19 -0400

    update triplet block algorithm, only need to work on the index

--- matlab/contribution_test.m
+++ matlab/contribution_test.m
@@ -3,7 +3,7 @@ rng(6);
 
 % create a random distance matrix that is symmetric with diagonal elements
 % equal to zeros
-n = 1000; b1 = 100; b2 = 200;
+n = 5; b1 = 100; b2 = 2;
 d = rand(n);
 %d = [0 1 2 3; 1 0 4 5; 2 4 0 6; 3 5 6 0];
 D = (d+d')/2;
@@ -24,6 +24,8 @@ toc
 
 % in the raw block method, the b should equal to sqrt(M)/3
 % where M is the cache size
+
+%{
 fprintf('block method, same dimension, using block size %d\n', b1)
 tic
 [C2,U] = pald_block(D,1,b1);
@@ -35,9 +37,19 @@ tic
 [C3,U1] = pald_opt(D,1,b2);
 toc 
 fprintf('error in optimal blocked method is %g\n', norm(C1-C3))
+%}
+
+
 
 fprintf('triplet method, same dimension\n')
 tic
 [C4,U4] = pald_triplet(D);
 toc 
 fprintf('error in triplet method is %g\n', norm(C1-C4))
+
+fprintf('triplet block method, same dimension\n')
+tic
+[C5,U5] = pald_triplet_block(D,b2);
+toc 
+fprintf('error in triplet method is %g\n', norm(C1-C5))
+
--- matlab/pald_triplet_block.m
+++ matlab/pald_triplet_block.m
@@ -14,17 +14,24 @@ C = zeros(n);
 % consider all unique triplet blocks to compute conflict focus sizes
 for x = 1:b:n
     Xb = x:min(x+b-1,n);
-    
-    
-    for y = x+b:b:n
+    for y = x:b:n
         Yb = y:min(y+b-1,n);
-       
-        % update size of conflict focus for block I,J of U
-        for z = y+b:b:n
-            Zb = z:min(z+b-1,n);
-              
+        for z = y:b:n
+            Zb = y:min(z+b-1,n);
+             
+            if x == y && y == z
+                index = 1;
+            elseif x == y
+                index = 2;
+            elseif x ~= y && y ~= z
+                index = 3;
+            else
+                continue;
+            end
+            
+            
             [U(Xb,Yb),U(Xb,Zb),U(Yb,Zb)] = ...
-            update_cfs(U(Xb,Yb),U(Xb,Zb),U(Yb,Zb),D(Xb,Yb),D(Xb,Zb),D(Yb,Zb));
+            update_cfs(U(Xb,Yb),U(Xb,Zb),U(Yb,Zb),D(Xb,Yb),D(Xb,Zb),D(Yb,Zb),index);
         end
     end
 end
@@ -32,7 +39,7 @@ end
 
 
 % fill in lower triangle of U
-U = U + U';
+% U = U + U';
 
 % initialize C with diagonal entries determined by row sums of 1/U
 C = diag(sum(1./(U+diag(Inf*ones(n,1))),2)); 
@@ -61,32 +68,104 @@ end
 
 
 
-function [Uxy,Uxz,Uyz] = update_cfs(Uxy,Uxz,Uyz,Dxy,Dxz,Dyz)
-% update conflict focus sizes for pairs in I,J block-pair 
-% based on vertices in K block
-    
+function [Uxy,Uxz,Uyz] = update_cfs(Uxy,Uxz,Uyz,Dxy,Dxz,Dyz,index)
+% update conflict focus sizes for triplets
+% if x == y == z 
+%    index = 1;
+% elseif x == y
+%    index = 2;
+% elseif x~=y & y~=z
+%    index = 3;
+% end
+ 
     [m,n] = size(Dxy); % m is the size of X block, n is the size of Y block
     [~,p] = size(Dxz); % p is the size of Z block
     
+    if index == 1
+        for x = 1:m
+            for y=(x+1):n
+                for z=(y+1):p
+                    
+                    if Dxy(x,y) < Dxz(x,z) && Dxy(x,y) < Dyz(y,z)     % xy is closest pair
+                        Uxz(x,z) = Uxz(x,z) + 1;
+                        Uyz(y,z) = Uyz(y,z) + 1;
+                    elseif Dxz(x,z) < Dxy(x,y) && Dxz(x,z) < Dyz(y,z) % xz is closest pair
+                        Uxy(x,y) = Uxy(x,y) + 1;
+                        Uyz(y,z) = Uyz(y,z) + 1;
+                    else                                      % yz is closest pair
+                        Uxy(x,y) = Uxy(x,y) + 1;
+                        Uxz(x,z) = Uxz(x,z) + 1;
+                    end
+                    
+                end
+            end
+        end
+    elseif index == 2
+        %{
+        
+        % two points from x and y, one point from z
+        for x = 1:m
+            for y=(x+1):n
+                for z=1:p
+                   
+                    if Dxy(x,y) < Dxz(x,z) && Dxy(x,y) < Dyz(y,z)     % xy is closest pair
+                        Uxz(x,z) = Uxz(x,z) + 1;
+                        Uyz(y,z) = Uyz(y,z) + 1;
+                    elseif Dxz(x,z) < Dxy(x,y) && Dxz(x,z) < Dyz(y,z) % xz is closest pair
+                        Uxy(x,y) = Uxy(x,y) + 1;
+                        Uyz(y,z) = Uyz(y,z) + 1;
+                    else                                      % yz is closest pair
+                        Uxy(x,y) = Uxy(x,y) + 1;
+                        Uxz(x,z) = Uxz(x,z) + 1;
+                    end
+                    
+                end
+            end
+        end
+        
+         % two points from z and one point from x or y
+        
+       for i = 1:p % i is the first point select from Z
+          for j = (z+1):p  % j is the second point select from Z
+               for k = 1:m % k is the first point select from X(Y)
+                    
+                    if Dxz(k,i) < Dxy(k,j) && Dxz(k,i) < Dyz(k,j) % ki is closest
+                        Uxy(k,j) = Uxy(k,j) + 1;
+                        Uyz(i,j) = Uyz(j,i) + 1;
+                    elseif Dxy(k,j) < Dxz(k,i) && Dxy(k,j) < Dyz(k,j) % kj is closest
+                        Uxy(k,i) = Uxy(k,j) + 1;
+                        Uyz(i,j) = Uyz(j,i) + 1;
+                    else                                      % ij is closest 
+                        Uxy(k,i) = Uxy(k,i) + 1;
+                        Uyz(k,j) = Uyz(k,j) + 1;
+                   
+                    end
+                    
+                end
+            end
+        end
+       
+        %}
     
-    for x = 1:m
-        % since x, y, z are different there won't be equal situation
-        for y=1:n
-            
-            for z=1:p
-                if Dxy(x,y) < Dxz(x,z) && Dxy(x,y) < Dyz(y,z)     % xy is closest pair
-                    Uxz(x,z) = Uxz(x,z) + 1;
-                    Uyz(y,z) = Uyz(y,z) + 1;
-                elseif Dxz(x,z) < Dxy(x,y) && Dxz(x,z) < Dyz(y,z) % xz is closest pair
-                    Uxy(x,y) = Uxy(x,y) + 1;
-                    Uyz(y,z) = Uyz(y,z) + 1;
-                else                                      % yz is closest pair
-                    Uxy(x,y) = Uxy(x,y) + 1;
-                    Uxz(x,z) = Uxz(x,z) + 1;
+    elseif index == 3
+        for x = 1:m
+            for y=1:n
+                for z=1:p
+                    if Dxy(x,y) < Dxz(x,z) && Dxy(x,y) < Dyz(y,z)     % xy is closest pair
+                        Uxz(x,z) = Uxz(x,z) + 1;
+                        Uyz(y,z) = Uyz(y,z) + 1;
+                    elseif Dxz(x,z) < Dxy(x,y) && Dxz(x,z) < Dyz(y,z) % xz is closest pair
+                        Uxy(x,y) = Uxy(x,y) + 1;
+                        Uyz(y,z) = Uyz(y,z) + 1;
+                    else                                      % yz is closest pair
+                        Uxy(x,y) = Uxy(x,y) + 1;
+                        Uxz(x,z) = Uxz(x,z) + 1;
+                    end
                 end
             end
         end
     end
+        
     
-
+        
 end
